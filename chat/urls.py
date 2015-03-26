d47fc952fe203b467040777d830062a3a7677267
cheney93 <823506023@qq.com>
2015-03-26 18:12:29 +0800

    解决了在线列表的问题

--- chat/urls.py
+++ chat/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns(
 	url(r'^(\d)$', views.room),
 	url(r'^getmsg/$', views.getmsg),
 	url(r'^putmsg/$', views.putmsg),
+	url(r'^exitchat/$', views.exituser),
+	url(r'^onlinelist/$', views.onlineuser),
 )
--- chat/views.py
+++ chat/views.py
@@ -16,18 +16,17 @@ def index(request):
 def room(request, roomid):
 	user = request.user
 	roomObj = ChatRoom.objects.get(id=roomid)
-	result = RoomAccount.objects.filter(username=user,roomname=roomObj)
+	result = RoomAccount.objects.filter(username=user, roomname=roomObj)
 	if not result:
-		u = RoomAccount(username=user,roomname=roomObj)
+		u = RoomAccount(username=user, roomname=roomObj)
 		u.save()
 	userlObj = RoomAccount.objects.filter(roomname=roomObj)
 	userlist = []
 	for i in userlObj:
 		userlist.append(i.username)
-	return render_to_response("chat/room.html", {'user': user, 'roomObj': roomObj, 'userlist':userlist})
+	return render_to_response("chat/room.html", {'user': user, 'roomObj': roomObj, 'userlist': userlist})
 
 
-@login_required(login_url='/account/login')
 def getmsg(request):
 	roomid = request.GET.get('roomid')
 	roomObj = ChatRoom.objects.get(id=roomid)
@@ -38,11 +37,33 @@ def getmsg(request):
 	return HttpResponse(json.dumps(msglist))
 
 
-@login_required(login_url='/account/login')
 def putmsg(request):
 	roomid, content = request.POST.get('roomid'), request.POST.get('content')
 	roomObj = ChatRoom.objects.get(id=roomid)
 	s = ChatPool(roomname=roomObj, msg=content)
 	s.save()
-	print ChatPool.objects.all()
 	return HttpResponse("OK")
+
+
+def exituser(request):
+	roomid, userid = request.POST.get('roomid'), request.POST.get('userid')
+	roomObj = ChatRoom.objects.get(id=roomid)
+	userObj = User.objects.get(id=userid)
+	u = RoomAccount.objects.filter(username=userObj, roomname=roomObj)
+	u.delete()
+	return HttpResponse("OK")
+
+
+def onlineuser(request):
+	roomid, userid = request.GET.get('roomid'), request.GET.get('userid')
+	roomObj = ChatRoom.objects.get(id=roomid)
+	userObj = User.objects.get(id=userid)
+	u = RoomAccount.objects.filter(username=userObj, roomname=roomObj)
+	if not u:
+		u = RoomAccount(username=userObj, roomname=roomObj)
+		u.save()
+	userlObj = RoomAccount.objects.filter(roomname=roomObj)
+	userlist = []
+	for i in userlObj:
+		userlist.append(str(i.username))
+	return HttpResponse(json.dumps(userlist))
